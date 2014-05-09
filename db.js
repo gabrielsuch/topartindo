a9c7b81ac67c24b10912e0b1af85ba872ff47bcf
Gabriel Such <gabrielsuch@gmail.com>
2014-05-09 15:30:07 -0300

    persist product

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var Product = new Schema({
   title: String,
   description: String,
-  price: Number,
+  price: Number
 });
 
 mongoose.model('Product', Product);
--- routes/products.js
+++ routes/products.js
@@ -9,4 +9,18 @@ router.get('/', function(req, res) {
   });
 });
 
+router.get('/new', function(req, res) {
+    res.render('product');
+});
+
+router.post('/', function(req, res) {
+    var data = req.body;
+    var product = new Product({ title: data.title, description: data.description, price: data.price });
+
+    product.save(function(err) {
+        if (err) return console.error(err);
+        res.send(product);
+    });
+});
+
 module.exports = router;
