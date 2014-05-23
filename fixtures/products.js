c0f7d0c6403918d980bfdbeb0b10a03c6e322fa9
Gabriel Such <gabrielsuch@gmail.com>
2014-05-23 17:48:13 -0300

    add about page

--- fixtures/products.js
+++ fixtures/products.js
@@ -1,7 +1,7 @@
 exports.Product = [
     { title: 'Peugeot 308 Allure 2.0 2014', description: '', price: 50000,   photo: 'carro.jpg' },
     { title: 'Micro-ondas Consul 30L',      description: '', price: 250,     photo: 'microondas.jpg' },
-    { title: 'Geladeira GE 445L',           description: '', price: 900,     photo: 'geladeira.jpg' },
+    { title: 'Geladeira GE 445L',           description: '', price: 1050,    photo: 'geladeira.jpg' },
     { title: 'iPhone 4 16Gb',               description: '', price: 600,     photo: 'iphone.jpg' },
     { title: 'Lavadora Brastemp 10Kg',      description: '', price: 650,     photo: 'lavadora.jpg' },
     { title: 'SmartTV Samsung LED 40',      description: '', price: 1100,    photo: 'tv.jpg' },
